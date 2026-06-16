cd4dfbcd11aaf27dd8ef88b41193e40a9c43e640
HappyWormy <m@atonua.com>
2026-06-16 14:05:50 +0300

    Anonymize config.example.php

--- config.example.php
+++ config.example.php
@@ -1,7 +1,7 @@
 <?php
 // Пример файла конфигурации. Скопируйте его в config.php и введите реальные данные.
 
-define('DB_HOST', 'wormy2.mysql.tools');
-define('DB_USER', 'wormy2_testcats');
-define('DB_PASSWORD', 'ВАШ_НОВЫЙ_ПАРОЛЬ');
-define('DB_NAME', 'wormy2_testcats');
+define('DB_HOST', 'ХОСТ_БАЗЫ_ДАННЫХ');
+define('DB_USER', 'ИМЯ_ПОЛЬЗОВАТЕЛЯ_БД');
+define('DB_PASSWORD', 'ПАРОЛЬ_БАЗЫ_ДАННЫХ');
+define('DB_NAME', 'ИМЯ_БАЗЫ_ДАННЫХ');
